347459dbf867c947f161f59fdb0a812e0489a313
khanyasar022 <38328077+khanyasar022@users.noreply.github.comyasarfarfat786@gmail.com>
2022-01-27 21:36:13 +0530

    added product component to see the product in detail

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import {Routes, Route} from 'react-router-dom'
 import Products from './components/Products';
+import Product from './components/Product';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home/>} />
         <Route exact path='/products' element={<Products/>} />
+        <Route exact path='/products/:id' element={<Product/>} />
       </Routes>
     </>
   );
